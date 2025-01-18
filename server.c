d21e160b58b2894872ccaa1e3d80661a385f1803
aymanelfadl <114816859+aymanelfadl@users.noreply.github.com>
2025-01-18 15:24:15 +0100

    hey

--- server.c
+++ server.c
@@ -1,116 +1,73 @@
-/* client.c */
+/* server.c */
 #include <signal.h>
 #include <unistd.h>
 #include <stdlib.h>
 
 typedef struct s_data
 {
-    int received;
-    int busy;
+    char    c;
+    int     bits;
+    pid_t   client_pid;
+    int     active;
 }   t_data;
 
 static t_data g_data;
 
-static void handle_int(int signum)
+static void reset_data(void)
 {
-    (void)signum;
-    ft_printf("\nTransmission interrupted. Exiting...\n");
-    exit(0);
+    g_data.c = 0;
+    g_data.bits = 0;
+    g_data.client_pid = 0;
+    g_data.active = 0;
 }
 
-static void handle_response(int signum, siginfo_t *info, void *context)
+static void handle_signal(int signum, siginfo_t *info, void *context)
 {
-    (void)info;
     (void)context;
-    
-    if (signum == SIGUSR1)
+
+    if (g_data.active && g_data.client_pid != info->si_pid)
     {
-        if (!g_data.received) // If we haven't received any acknowledgment yet
+        kill(info->si_pid, SIGUSR1);
+        return;
+    }
+    if (!g_data.active)
+    {
+        g_data.client_pid = info->si_pid;
+        g_data.active = 1;
+    }
+
+    if (signum == SIGUSR1)
+        g_data.c = (g_data.c << 1) | 1;
+    else if (signum == SIGUSR2)
+        g_data.c <<= 1;
+
+    g_data.bits++;
+    if (g_data.bits == 8)
+    {
+        if (g_data.c == '\0')
         {
-            g_data.busy = 1;  // Server is busy with another client
+            kill(g_data.client_pid, SIGUSR1);
+            reset_data();
             return;
         }
-        ft_printf("\nMessage transmitted successfully!\n");
-        exit(0);
+        write(1, &g_data.c, 1);
+        g_data.c = 0;
+        g_data.bits = 0;
     }
-    else if (signum == SIGUSR2)
-        g_data.received = 1;
+    kill(g_data.client_pid, SIGUSR2);
 }
 
-static int wait_response(void)
-{
-    int timeout;
-
-    timeout = 0;
-    while (!g_data.received && !g_data.busy)
-    {
-        if (timeout++ > 1000)
-        {
-            ft_printf("Error: Server not responding\n");
-            return (-1);
-        }
-        usleep(100);
-    }
-    
-    if (g_data.busy)
-    {
-        ft_printf("Error: Server is busy with another client. Please try again later.\n");
-        return (-1);
-    }
-    
-    return (0);
-}
-
-static int send_char(char c, pid_t server_pid)
-{
-    int bit;
-
-    bit = 7;
-    while (bit >= 0)
-    {
-        g_data.received = 0;
-        if ((c >> bit) & 1)
-            kill(server_pid, SIGUSR1);
-        else
-            kill(server_pid, SIGUSR2);
-        
-        if (wait_response() == -1)
-            return (-1);
-        bit--;
-    }
-    return (0);
-}
-
-int main(int argc, char *argv[])
+int main(void)
 {
     struct sigaction sa;
-    pid_t server_pid;
-    int i;
 
-    if (argc != 3)
-    {
-        ft_printf("Usage: %s <server_pid> <message>\n", argv[0]);
-        exit(1);
-    }
-    server_pid = atoi(argv[1]);
-    if (server_pid <= 0)
-    {
-        ft_printf("Error: Invalid server PID\n");
-        exit(1);
-    }
+    reset_data();
+    ft_printf("Server PID: %d\n", getpid());
 
-    // Initialize data
-    g_data.received = 0;
-    g_data.busy = 0;
-
-    // Set up SIGINT handler
-    signal(SIGINT, handle_int);
-
-    // Set up response handlers
     sigemptyset(&sa.sa_mask);
-    sa.sa_sigaction = handle_response;
+    sa.sa_sigaction = handle_signal;
     sa.sa_flags = SA_SIGINFO | SA_RESTART;
-    
+
     if (sigaction(SIGUSR1, &sa, NULL) == -1 || 
         sigaction(SIGUSR2, &sa, NULL) == -1)
     {
@@ -118,17 +75,7 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    ft_printf("Sending message to server (PID: %d)...\n", server_pid);
-    i = 0;
-    while (argv[2][i])
-    {
-        if (send_char(argv[2][i], server_pid) == -1)
-            exit(1);
-        i++;
-    }
-    send_char('\0', server_pid);
-    
-    // Wait for final acknowledgment
-    usleep(1000);
+    while (1)
+        pause();
     return (0);
 }
