efe8683e941074d59b8b418ee8681d98b09f4a53
Ayman Elfadl <aelfadl@e2r3p5.1337.ma>
2025-01-18 22:08:30 +0100

    hey

--- client.c
+++ client.c
@@ -1,90 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aelfadl <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/18 21:02:10 by aelfadl           #+#    #+#             */
+/*   Updated: 2025/01/18 21:02:22 by aelfadl          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft/libft.h"
 #include <signal.h>
 #include <unistd.h>
 #include <stdlib.h>
 
-int done;
+int	g_done;
 
-static void handle_int(int signum)
+static	void	handle_response(int signum, siginfo_t *info, void *context)
 {
-    (void)signum;
-    ft_printf("Can't stop now ...\n");
+	(void)info;
+	(void)context;
+	if (signum == SIGUSR1)
+	{
+		if (!g_done)
+		{
+			ft_printf("bro wait me to finish koon t7chem chi chewi!\n");
+			return ;
+		}
+		ft_printf("\nMessage transmitted successfully!\n");
+		exit(0);
+	}
+	else if (signum == SIGUSR2)
+		g_done = 1;
 }
 
-static void handle_response(int signum, siginfo_t *info, void *context)
+static	int	wait_response(void)
 {
-    (void)info;
-    (void)context;
-    
-    if (signum == SIGUSR1)
-    {
-        if (!done)
-        {
-            ft_printf("bro wait me to finish koon t7chem chi chewi!\n");
-            return;
-        }
-        ft_printf("\nMessage transmitted successfully!\n");
-        exit(0);
-    }
-    else if (signum == SIGUSR2)
-        done = 1;
+	int	timeout;
+
+	timeout = 0;
+	while (!g_done)
+	{
+		if (timeout++ > 1000)
+			return (ft_printf("Error: Server not responding\n"), -1);
+		usleep(100);
+	}
+	return (0);
 }
 
-static int wait_response(void)
+static	int	send_char(char c, pid_t server_pid)
 {
-    int timeout;
+	int	bit;
 
-    timeout = 0;
-    while (!done)
-    {
-        if (timeout++ > 1000)
-        {
-            ft_printf("Error: Server not responding\n");
-            return (-1);
-        }
-        usleep(100);
-    }
-    
-    return (0);
+	bit = 7;
+	while (bit >= 0)
+	{
+		g_done = 0;
+		if ((c >> bit) & 1)
+			kill(server_pid, SIGUSR1);
+		else
+			kill(server_pid, SIGUSR2);
+		if (wait_response() == -1)
+			exit(1);
+		bit--;
+	}
+	return (0);
 }
 
-static int send_char(char c, pid_t server_pid)
+int	main(int argc, char *argv[])
 {
-    int bit;
+	struct sigaction		sa;
+	int						i;
 
-    bit = 7;
-    while (bit >= 0)
-    {
-        done = 0;
-        if ((c >> bit) & 1)
-            kill(server_pid, SIGUSR1);
-        else
-            kill(server_pid, SIGUSR2);
-        if (wait_response() == -1)
-            exit(1);
-        bit--;
-    }
-    return (0);
-}
-
-int main(int argc, char *argv[])
-{
-    struct sigaction sa;
-    int i;
-
-    if (argc != 3)
-        return(ft_printf("Usage: %s <server_pid> <message>\n", argv[0]), 1);
-    if (atoi(argv[1]) <= 0)
-        return (ft_printf("Error: Invalid server PID\n"), 1);
-    signal(SIGINT, handle_int);
-    sigemptyset(&sa.sa_mask);
-    sa.sa_sigaction = handle_response;
-    sa.sa_flags = SA_SIGINFO;
-    if (sigaction(SIGUSR1, &sa, NULL) == -1 || 
-        sigaction(SIGUSR2, &sa, NULL) == -1)
-        return (ft_printf("Error: Failed to setup signal handlers\n"), 1);
-    i = 0;
-    while (argv[2][i])
-        send_char(argv[2][i++], atoi(argv[1]));
-    send_char('\0', atoi(argv[1]));
+	if (argc != 3)
+		return (ft_printf("Usage: %s <server_pid> <message>\n", argv[0]), 1);
+	if (atoi(argv[1]) <= 0)
+		return (ft_printf("Error: Invalid server PID\n"), 1);
+	sigemptyset(&sa.sa_mask);
+	sa.sa_sigaction = handle_response;
+	sa.sa_flags = SA_SIGINFO;
+	if (sigaction(SIGUSR1, &sa, NULL) == -1
+		|| sigaction(SIGUSR2, &sa, NULL) == -1)
+		return (ft_printf("Error: Failed to setup signal handlers\n"), 1);
+	i = 0;
+	while (argv[2][i])
+		send_char(argv[2][i++], atoi(argv[1]));
+	send_char('\0', atoi(argv[1]));
+	return (0);
 }
--- server.c
+++ server.c
@@ -1,76 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aelfadl <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/18 21:02:30 by aelfadl           #+#    #+#             */
+/*   Updated: 2025/01/18 21:02:32 by aelfadl          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <signal.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include "libft/libft.h"
 
-typedef struct s_data
+static void reset_data(int *active, int *client_pid, char *c, int *bits)
 {
-    char    c;
-    int     bits;
-    int     client_pid;
-    int     active;
-}   t_data;
-
-static t_data g_data;
-
-static void reset_data(void)
-{
-    g_data.c = 0;
-    g_data.bits = 0;
-    g_data.client_pid = 0;
-    g_data.active = 0;
+    *c = 0;
+    *bits = 0;
+    *client_pid = 0;
+    *active = 0;
 }
 
 static void handle_signal(int signum, siginfo_t *info, void *context)
 {
     (void)context;
-    if (g_data.active && g_data.client_pid != info->si_pid)
-        reset_data();
+    static int active;
+    static int client_pid;
+    static char c;
+    static int bits;
 
-    if (!g_data.active)
+    if (active && client_pid != info->si_pid)
+        reset_data(&active, &client_pid, &c, &bits);
+    if (!active)
     {
-        g_data.client_pid = info->si_pid;
-        g_data.active = 1;
+        client_pid = info->si_pid;
+        active = 1;
     }
     if (signum == SIGUSR1)
-        g_data.c = (g_data.c << 1) | 1;
+        c = (c << 1) | 1;
     else if (signum == SIGUSR2)
-        g_data.c <<= 1;
-
-    g_data.bits++;
-    if (g_data.bits == 8)
+        c <<= 1;
+    bits++;
+    if (bits == 8)
     {
-        if (g_data.c == '\0')
+        if (c == '\0')
         {
-            kill(g_data.client_pid, SIGUSR1);
-            reset_data();
+            kill(client_pid, SIGUSR1);
+            reset_data(&active, &client_pid, &c, &bits);
             return;
         }
-        write(1, &g_data.c, 1);
-        g_data.c = 0;
-        g_data.bits = 0;
+        write(1, &c, 1);
+        c = 0;
+        bits = 0;
     }
-    kill(g_data.client_pid, SIGUSR2);
+    kill(client_pid, SIGUSR2);
 }
 
 int main(void)
 {
     struct sigaction sa;
 
-    reset_data();
     ft_printf("Server PID: %d\n", getpid());
-
     sigemptyset(&sa.sa_mask);
     sa.sa_sigaction = handle_signal;
     sa.sa_flags = SA_SIGINFO | SA_RESTART;
 
     if (sigaction(SIGUSR1, &sa, NULL) == -1 || 
         sigaction(SIGUSR2, &sa, NULL) == -1)
-    {
-        ft_printf("Error: Failed to setup signal handlers\n");
-        exit(1);
-    }
-
+        return (ft_printf("Error: Failed to setup signal handlers\n"), 1);
     while (1)
         pause();
     return (0);
